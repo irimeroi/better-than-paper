d729f53af2fd2cec4a77586827db76672f20629b
Irina Meroi <irimeroi7@gmail.com>
2023-11-05 16:51:42 -0800

    add code for notes to work

--- server.js
+++ server.js
@@ -1,45 +1,63 @@
 const express = require('express');
 // path is a built-in Node.js package
 const path = require('path');
-const notesDb = require('./db/notes.json')
+const fs = require('fs');
+const uuid = require('./helpers/uuid.js')
 
 //initialized express
 const app = express();
-
 //express runs on specified port
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 //static middleware pointing to the public folder
 app.use(express.static('public'));
-
+//lines for every app - tell the server we're passing json
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
 //linking static files to the server
-app.get('/index', (req, res) => 
+//homepage
+app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, 'public/index.html'))
 );
+//notespage
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
 //get request for all notes
-app.get('/api/database', (req, res) => {
-    console.info(`${req.method} request received to get notes`)
-    return res.json(notesDb)
+app.get('/api/notes', (req, res) => {
+    const notes = require('./db/notes.json');
+    return res.json(notes)
 });
 
 //post request to add a note
-app.post('/api/database', (req, res) => {
-    console.info(`${req.method} request received to add a review`);
-    let response;
-
-    if (req.body && req.body.title) {
-        response = {
-            data: req.body
+app.post('/api/notes', (req, res) => {
+    const notes = require('./db/notes.json');
+    const { title, text } = req.body;
+    const newNote = {
+        title,
+        text,
+        id: uuid(),
+    };
+    notes.push(newNote);
+    fs.writeFile('./db/notes.json', JSON.stringify(notes, null, 2), (err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send(201).end();
+            // res.json(notes)
         }
-    }
-    res.json(response)
+    });
+})
+
+app.delete('api/notes/:id', (req, res) => {
+    // const notes = require('./db/notes.json');
+    // notes.filter(req.params.id);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
 //listen() listens for incoming connections on the specified port
